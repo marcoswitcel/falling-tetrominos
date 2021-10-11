a149f89dbe96696937a23fda8cb8438280852d14
João Marcos <joao@agenciagh.com.br>
2021-10-10 21:24:20 -0300

    alpha channel default value adjusted

--- colors.js
+++ colors.js
@@ -1,25 +1,25 @@
 //@ts-check
 
 /**
- * @param {number} r Entre 0 e 255
- * @param {number} g Entre 0 e 255
- * @param {number} b Entre 0 e 255
- * @param {number?} a Entre 0 e 255
+ * @param {number} r Entre 0 e 255 (integer)
+ * @param {number} g Entre 0 e 255 (integer)
+ * @param {number} b Entre 0 e 255 (integer)
+ * @param {number?} a Entre 0 e 1 (flot)
  * 
  * @return {RGBA}
  */
-export function rgba(r, g, b, a = 255) {
+export function rgba(r, g, b, a = 1) {
     return new RGBA(r, g, b, a);
 }
 
 export class RGBA {
     /**
-     * @param {number} r Entre 0 e 255
-     * @param {number} g Entre 0 e 255
-     * @param {number} b Entre 0 e 255
-     * @param {number?} a Entre 0 e 255
+     * @param {number} r Entre 0 e 255 (integer)
+     * @param {number} g Entre 0 e 255 (integer)
+     * @param {number} b Entre 0 e 255 (integer)
+     * @param {number?} a Entre 0 e 1 (flot)
      */
-    constructor(r, g, b, a = 255) {
+    constructor(r, g, b, a = 1) {
         this.r = r;
         this.g = g;
         this.b = b;
