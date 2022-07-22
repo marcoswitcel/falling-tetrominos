c41a76c67a8737b684f6477b4a5507fa9eefb046
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-07-21 21:05:40 -0300

    refatorando: movendo block manager para outro arquivo

---
+++ tetris-block-manager.js
@@ -0,0 +1,74 @@
+
+export class TetrisBlockManager {
+
+    static getRandomBlockMatrix() {
+        const blocks = ['T', 'L', 'I', 'B', 'J', 'Z', 'ZI'];
+        return JSON.parse(
+            JSON.stringify(
+                this[blocks[blocks.length * Math.random() | 0]]
+            )
+        );
+    }
+
+    static T = {
+        code: 1,
+        matrix: [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 1, 0],
+        ],
+    };
+
+    static L = {
+        code: 2,
+        matrix: [
+            [0, 0, 0, 0],
+            [0, 2, 0, 0],
+            [0, 2, 0, 0],
+            [0, 2, 2, 0],
+        ],
+    };
+
+
+    static I = {
+        code: 3,
+        matrix: [
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 0],
+        ],
+    };
+
+    static B = {
+        code: 4,
+        matrix: [
+            [4, 4],
+            [4, 4],
+        ],
+    };
+
+    static J = {
+        code: 5,
+        matrix: [
+            [0, 5],
+            [0, 5],
+            [5, 5],
+        ],
+    };
+
+    static Z = {
+        code: 6,
+        matrix: [
+            [6, 6, 0],
+            [0, 6, 6],
+        ],
+    };
+
+    static ZI = {
+        code: 7,
+        matrix: [
+            [0, 7, 7],
+            [7, 7, 0],
+        ],
+    };
+}
