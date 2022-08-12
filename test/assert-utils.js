44509b788232ffeca4442533bf386e4d4f746b1c
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-08-11 23:06:02 -0300

    documentação assert

--- test/assert-utils.js
+++ test/assert-utils.js
@@ -1,3 +1,10 @@
+
+/**
+ * Função usada para reportar o resultado de cada cenário
+ * @param {boolean} condition resultado da condição aplicada
+ * @param {string} description Descrição apresentada ao usuário em caso de
+ * sucesso ou falha.
+ */
 export const assert = (condition, description) => {
     console.log(`${condition ? '✅' : '❌'}: ${description}`);
 };
