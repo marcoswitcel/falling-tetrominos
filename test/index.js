e88b0a3024bb37da6b34199a43cc8f6fa64f9648
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-08-13 22:30:38 -0300

    ideas

--- test/index.js
+++ test/index.js
@@ -8,6 +8,12 @@ import { NodeElementTest } from '../window/node-element.test.js';
  * @todo João, refatorar esse trecho para fazer uso de uma função executora
  * para agregar informações e garantir que os testes não sejam interrompidos
  * por falhas em testes anteriores.
+ * @todo João, talvez deveria trazer os resultados colapsado, apresentar de uma
+ * forma mais compacta.
+ * @todo João, considerar algum mecanismo de contagem de testes por
+ * agrupamento. Talvez no nome do teste deveria aparecer uma contagem de
+ * substestes. Será necessário incrementar a forma de controle dos testes
+ * executados.
  */
 StorageUtilityTest.run();
 TickerTest.run();
