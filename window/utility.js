d00f1b7341181a0dc413f1ede0d3d4a513dae92b
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-08-14 12:55:12 -0300

    fx: ajuste documentação

--- window/utility.js
+++ window/utility.js
@@ -8,9 +8,10 @@
  * @param {object} object objeto que terá o campo promovido a apenas leitura
  * @param {string} field campo que já existe no objeto e será configurado
  * como apenas leitura
- * @param {boolean} [configurable] Parêmtro que define se a configuração pode
+ * @param {boolean} [configurable] Paramêtro que define se a configuração pode
  * ser ajustada no futuro, por padrão `false`, o que significa que o campo não
  * pode ser reajustado uma vez criado.
+ * @returns {void}
  */
 export function makeFieldReadOnly(object, field, configurable = false) {
     Object.defineProperty(object, field, {
